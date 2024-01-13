64daeda6baddceb20de159f41e1a2d35da46449b
Azim Afnaan <azim.afnaan@hotmail.com>
2024-01-13 20:13:40 +0600

    emplement xpath

--- cypress/e2e/XpathLocators.cy.js
+++ cypress/e2e/XpathLocators.cy.js
@@ -1,5 +1,11 @@
 describe("XpathLOcators", () => {
     it("xpathLocators", () => {
-        cy.visit("http://www.automationpractice.pl/index.php")
+        cy.visit("http://www.automationpractice.pl/")
+        cy.xpath("//ul[@id='blockbestsellers']/li").should('have.length', 7)
+    })
+
+    it("chained xpath", () => {
+        cy.visit("http://www.automationpractice.pl/")
+        cy.xpath("//ul[@id='blockbestsellers']").xpath("/li").should('have.length', 7)
     })
 })
