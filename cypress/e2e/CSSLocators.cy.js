2e59cf95dd10ef41637cf3a623ddb5ec1f432e15
Azim Afnaan <azim.afnaan@hotmail.com>
2024-01-13 19:06:27 +0600

    Check the assertions value true or false

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -2,10 +2,11 @@ describe("CSSLocators", () => {
     it(cssLocators, () => {
         cy.visit("http://www.automationpractice.pl/index.php")
 
-        cy.get("#search_query_top").type("T-Shirts") //Id 
-        cy.get(".search_query").type("T-Shirt") //Class 
-        cy.get("[name='search_query']").type("T-Shirt") //attribute 
+        // cy.get("#search_query_top").type("T-Shirts") //Id 
+        // cy.get(".search_query").type("T-Shirt") //Class 
+        // cy.get("[name='search_query']").type("T-Shirt") //attribute 
         cy.get("input.search_query[name='search_query']").type("T-Shirt") //Tag, Class & attribute
+        cy.get("[name='submit_search']").click()
         cy.get(".lighter").contains("T-Shirts") //Assertions
     })
 })
