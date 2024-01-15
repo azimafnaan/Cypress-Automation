072629ba5d79c53bba21bb919e4b905e55d94c46
Azim Afnaan <azim.afnaan@hotmail.com>
2024-01-16 01:48:40 +0600

    Interacting with Auto Suggest DropDowns Elements

--- cypress/e2e/DropDowns.cy.js
+++ cypress/e2e/DropDowns.cy.js
@@ -1,5 +1,3 @@
-
-
 describe("Handle Dropdown", () => {
     it.skip("Dropdown with Select Tag", () => {
         Cypress.on('uncaught:exception', (err, runnable) => {
@@ -14,11 +12,19 @@ describe("Handle Dropdown", () => {
             .should("have.value", "India")
     })
 
-    it("DropDown without Select( Bootstrap)", () => {
+    it.skip("DropDown without Select( Bootstrap)", () => {
         cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
         cy.get("#select2-billing_country-container").click()
         cy.get(".select2-search__field").type("Italy").type('{enter}')
         cy.get("#select2-billing_country-container")
+            //Assertion
             .should('have.text', 'Italy')
+    })
+
+    it("Auto Suggest Dropdown", () => {
+        cy.visit("https://www.wikipedia.org/");
+        cy.get("#searchInput").type('Bangladesh')
+        cy.get(".suggestion-title").contains("Bangladesh Army").click()
+        cy.get('.mw-page-title-main').should('have.text', 'Bangladesh Army')
     })
 })
