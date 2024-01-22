5eb122e49f6e7dd3cd971237869e5de9fb32bf4c
Azim Afnaan <azim.afnaan@hotmail.com>
2024-01-22 18:45:39 +0600

    handle js confirm alert

--- README.md
+++ README.md
@@ -29,3 +29,8 @@ Interacting with Elements
             -DropDown without Select( Bootstrap)
             -Auto suggest DropDown
             -Dynamic Drop Down
+
+    Handle different types of alerts in Cypres
+
+            -JavaScript Alert
+            -JavaScript Confirm Alert
--- cypress/e2e/Alerts.cy.js
+++ cypress/e2e/Alerts.cy.js
@@ -1,17 +1,39 @@
 describe('Alerts', () => {
     //1) Javascript Alert: It will have some text and an 'OK' button
 
-    it('Js Alert', () => {
+    it.skip('Js Alert', () => {
         cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
         cy.get("button[onclick='jsAlert()']").click();
         cy.on('window:alert', (t) => {
-            expect(t).to.contain('I am a JS Alert')
+            expect(t).to.contains('I am a JS Alert')
         })
+        // alert window automatically close by cypress
+        cy.get("#result").should('have.text', 'You successfully clicked an alert')
     })
-
 
     //2) Javascript Confirm: It will have some text with 'OK' and 'Cancel' button
 
+    it.skip('Js Confirm Alert - Ok', () => {
+        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+        cy.get("button[onclick='jsConfirm()']").click();
+        cy.on('window:confirm', (t) => {
+            expect(t).to.contains('I am a JS Confirm')
+        })
+        // cypress automatically closed alert window by using ok button-default
+        cy.get("#result").should('have.text', 'You clicked: Ok')
+    })
+
+    it.only('Js Confirm Alert - cancel', () => {
+        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+        cy.get("button[onclick='jsConfirm()']").click();
+        cy.on('window:confirm', (t) => {
+            expect(t).to.contains('I am a JS Confirm')
+        })
+        //cypress closes alert window using cancel button
+        cy.on('window:confirm', () => false)
+        // cypress automatically closed alert window by using ok button-default
+        cy.get("#result").should('have.text', 'You clicked: Cancel')
+    })
     //3) Javascript Prompt Alert: It will have some text with a text box for user input along with 'OK' and 'Cancel' button
 
     //4) Authenticated Alert 
