1794b1da8ef0a5d4025aa8f52b4ac0c07a606fa2
Azim Afnaan <azim.afnaan@hotmail.com>
2024-01-30 01:03:19 +0600

    Read all the rows & columns data in the first page

--- cypress/e2e/Table.cy.js
+++ cypress/e2e/Table.cy.js
@@ -16,11 +16,18 @@ describe("Handle Tables", () => {
         cy.get('table[class="table table-bordered table-hover"]>tbody>tr').should('have.length', '10');
         cy.get('table[class="table table-bordered table-hover"]>thead>tr>td').should('have.length', '7');
     })
-    it.only('Check cell data from specific row and column', () => {
+    it.skip('Check cell data from specific row and column', () => {
         cy.get('table[class="table table-bordered table-hover"]>tbody>tr:nth-child(4)>td:nth-chi(3)').contains("gorankrezic90@gmail.com")
     })
-    it('Read all the rows & columns data in the first page', () => {
-
+    it.only('Read all the rows & columns data in the first page', () => {
+        cy.get('table[class="table table-bordered table-hover"]>tbody>tr')
+            .each(($row, index, $rows) => {
+                cy.wrap($row).within(() => {
+                    cy.get('td').each(($col, index, $cols) => {
+                        cy.log($col.text());
+                    })
+                })
+            })
     })
     it.skip('Pagination', () => {
 
