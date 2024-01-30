d74acab1af81ad6c67cf62d58fb9474c7288ed69
Azim Afnaan <azim.afnaan@hotmail.com>
2024-01-31 02:48:48 +0600

    install cypress iframe

--- cypress/e2e/MouseOperation.cy.js
+++ cypress/e2e/MouseOperation.cy.js
@@ -1,3 +1,4 @@
+import "cypress-iframe"
 describe("Mouse Operation", () => {
 
     it.skip("Mouse Hover", () => {
@@ -8,7 +9,7 @@ describe("Mouse Operation", () => {
         cy.get("div[class='dropdown-menu show'] li:nth-child(1) a:nth-child(1)").should("be.visible");
     })
 
-    it.only("Right Click", () => {
+    it.skip("Right Click", () => {
         cy.visit("https://swisnl.github.io/jQuery-contextMenu/demo.html")
         cy.wait(300)
         // Approach -1
@@ -23,7 +24,13 @@ describe("Mouse Operation", () => {
 
     })
 
-    it("Double Click", () => {
+    it.only("Double Click", () => {
+        cy.visit("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick")
+        cy.frameLoaded("#iframeResult");
+        // Approach-1
+        cy.iframe("#iframeResult").find("button[ondblclick='myFunction()']").trigger("dblclick")
+        cy.wait(5000);
+        cy.iframe("#iframeResult").find("#demo").contains("Hello World")
 
     })
 
