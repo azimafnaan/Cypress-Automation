c642c8cfca5e2d0b98cec483f3eb6a6e2f324e4b
Azim Afnaan <azim.afnaan@hotmail.com>
2024-02-01 02:38:46 +0600

    install cypress file upload

---
+++ cypress/e2e/FileUpload.cy.js
@@ -0,0 +1,21 @@
+describe("File Uploads", () => {
+    it("Single File Upload", () => {
+
+    })
+
+    it("File Upload - Rename", () => {
+
+    })
+
+    it("File Upload - Drag and Drop", () => {
+
+    })
+
+    it("Multiple File Upload", () => {
+
+    })
+
+    it("File Upload - Shadow Dom", () => {
+
+    })
+})
