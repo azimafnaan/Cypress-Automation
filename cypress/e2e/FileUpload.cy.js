79bd555a66cf2c93bf5e68ef0344d1c1444cb1f2
Azim Afnaan <azim.afnaan@hotmail.com>
2024-02-01 02:56:53 +0600

    Single File Upload

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -1,6 +1,14 @@
-describe("File Uploads", () => {
-    it("Single File Upload", () => {
+import "cypress-file-upload";
 
+describe("File Uploads", () => {
+    it.only("Single File Upload", () => {
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        cy.get("#file-upload").attachFile('test1.pdf');
+        cy.get("#file-submit").click();
+        cy.wait(5000);
+        cy.get(".example>h3").should("have.text", "File Uploaded!")
+        cy.wait(3000);
+        cy.get("#uploaded-files").contains("test1.pdf");
     })
 
     it("File Upload - Rename", () => {
