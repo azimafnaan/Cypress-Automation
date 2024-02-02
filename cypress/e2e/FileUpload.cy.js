17ae14db076cd0fce74bc36f58d85aae6eb9d3e5
Azim Afnaan <azim.afnaan@hotmail.com>
2024-02-02 17:12:25 +0600

    file upload - rename

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -1,7 +1,7 @@
 import "cypress-file-upload";
 
 describe("File Uploads", () => {
-    it.only("Single File Upload", () => {
+    it("Single File Upload", () => {
         cy.visit("https://the-internet.herokuapp.com/upload")
         cy.get("#file-upload").attachFile('test1.pdf');
         cy.get("#file-submit").click();
@@ -11,8 +11,14 @@ describe("File Uploads", () => {
         cy.get("#uploaded-files").contains("test1.pdf");
     })
 
-    it("File Upload - Rename", () => {
-
+    it.only("File Upload - Rename", () => {
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        cy.get("#file-upload").attachFile({ filePath: 'test1.pdf', fileName: 'myfile.pdf' });
+        cy.get("#file-submit").click();
+        cy.wait(5000);
+        cy.get(".example>h3").should("have.text", "File Uploaded!")
+        cy.wait(3000);
+        cy.get("#uploaded-files").contains("myfile.pdf");
     })
 
     it("File Upload - Drag and Drop", () => {
